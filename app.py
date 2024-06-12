8e3db903aad4166bf0f6c1998e2d5d080daf244a
Mohamed Hussein <150954700+MohamedHussein811@users.noreply.github.com>
2024-06-12 11:43:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import numpy as np
-from tensorflow.keras.models import load_model
-from io import BytesIO
 from PIL import Image
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+import tensorflow as tf
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
@@ -14,54 +12,56 @@ models = {
     "inceptionV3": None,
     "Xception": None,
     "cnnscratch": None,
-    "vgg19":None,
+    "vgg19": None,
 }
 
 model_paths = {
-    "denseNetModel": "best_denseNet_model.h5",
-    "inceptionV3": "best_inception_V3_model.h5",
-    "Xception": "best_Xception_model.h5",
-    "cnnscratch": "scratch.h5",
-    "vgg19":"best_vgg16_model.h5",}
+    "denseNetModel": "best_denseNet_model.tflite",
+    "inceptionV3": "best_inception_V3_model.tflite",
+    "Xception": "best_Xception_model.tflite",
+    "cnnscratch": "scratch.tflite",
+    "vgg19": "best_vgg16_model.tflite",
+}
 
-def load_keras_models():
+def load_tf_lite_models():
     global models
     for key, path in model_paths.items():
         if models[key] is None:
             print(f"Loading {key} from {path}")
-            models[key] = load_model(path)
+            models[key] = tf.lite.Interpreter(model_path=path)
+            models[key].allocate_tensors()
             print(f"{key} loaded")
 
 def process_and_predict_image(file, model_key):
-    img = Image.open(BytesIO(file.read()))
+    img = Image.open(file)
     img = img.resize((150, 150))
-    img_array = preprocess_input(np.array(img))
-    img_array = img_array.reshape(1, 150, 150, 3)
+    img_array = np.array(img) / 255.0  # Normalize image
 
-    predictions = models[model_key].predict(img_array)
+    input_details = models[model_key].get_input_details()
+    output_details = models[model_key].get_output_details()
 
-    predicted_class = np.argmax(predictions, axis=1)
-    prediction_percentages = (predictions[0] * 100).tolist()
+    input_shape = input_details[0]['shape']
+    models[model_key].set_tensor(input_details[0]['index'], [img_array])
+
+    models[model_key].invoke()
+    output_data = models[model_key].get_tensor(output_details[0]['index'])
+
+    predicted_class = np.argmax(output_data, axis=1)
+    prediction_percentages = (output_data[0] * 100).tolist()
 
     return predicted_class[0], prediction_percentages
-
-@app.route('/model_summary/<model_key>')
-def model_summary(model_key):
-    summary_output = []
-    models[model_key].summary(print_fn=lambda x: summary_output.append(x))
-    return "\n".join(summary_output)
 
 @app.route('/predict/<model_key>', methods=['POST'])
 def predict(model_key):
     file = request.files['file']
     predicted_class, prediction_percentages = process_and_predict_image(file, model_key)
-    print(model_key)
     return jsonify({
         'predicted_class': int(predicted_class),
         'prediction_percentages': prediction_percentages
     })
+
 if __name__ == '__main__':
-    load_keras_models() 
+    load_tf_lite_models()
     app.run()
 
 # Add the line below for Vercel to recognize the app
